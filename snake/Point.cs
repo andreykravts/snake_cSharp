53ef5ece21c43017107d2b28284046d6e9da4d9d
Andrey Kravtsov <63850989+andreykravts@users.noreply.github.com>
2021-04-12 22:51:27 +0300

    Initial commit Lesson3 Class point

---
+++ snake/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_OPP_Csharp
+{
+    public class Point
+    {
+        //this part of code make a dot in the place that the want to put it
+
+        public int x;
+        public int y;
+        public char sym;
+        //end part code
+
+        /// <summary>
+        /// Function for print a dot in the position that we will put through
+        /// this function use values that is dirived to parametrs in our class from outside
+        /// here we use parametrs from this class
+        /// </summary>
+        /// <param name="x">x position on window(integer)</param>
+        /// <param name="y">y position on window(integer)</param>
+        /// <param name="sym">The symbol that we gonna mark our dot(char)</param>
+        public void Draw()
+        {
+            //this part of code make a dot in the place that the want to put it
+
+            Console.SetCursorPosition(x, y); // position
+            Console.Write(sym);
+            //end part code
+        }
+    }
+}
---
+++ snake/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Snake_OPP_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            //put new values
+            p1.x = 1;//we send values to the parametrs in the class point
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();// now we dint put a values into method because we use parametrs from class point
+                      // print the dot 
+
+
+
+
+
+            ////this part of code make a dot in the place that the want to put it
+            ////another dot
+            Point p2 = new Point();
+            //put new values
+            p2.x = 5; //we send values to the parametrs in the class point
+            p2.y = 4;
+            p2.sym = '#';
+            p1.Draw(); // now we dint put a values into method because we use parametrs from class point
+            // print the dot 
+            ////end part code
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
