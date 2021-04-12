e5bfb0138163f99295b6e78b3cbb9d512ff00ee5
Andrey Kravtsov <63850989+andreykravts@users.noreply.github.com>
2021-04-12 22:51:27 +0300

    Initial commit Lesson4 Class point: constructor and part of incapsulation

---
+++ snake/Point.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_OPP_Csharp
+{
+    public class Point
+    {
+        //this part of code make a dot in the place that the want to put it
+
+        public int x;
+        public int y;
+        public char sym;
+        //end part code
+
+        
+        /// <summary>
+        /// constructor empty by default should use it....
+        /// </summary>
+        public Point()
+        {
+        }
+
+        /// <summary>
+        /// New constructor isnt empty here we have working part (initialization)
+        /// </summary>
+        /// <param name="_x">input</param>
+        /// <param name="_y">input</param>
+        /// <param name="_sym">input</param>
+       
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        /// <summary>
+        /// Function for print a dot in the position that we will put through
+        /// this function use values that is dirived to parametrs in our class from outside
+        /// here we use parametrs from this class
+        /// </summary>
+        /// <param name="x">x position on window(integer)</param>
+        /// <param name="y">y position on window(integer)</param>
+        /// <param name="sym">The symbol that we gonna mark our dot(char)</param>
+        public void Draw()
+        {
+            //this part of code make a dot in the place that the want to put it
+
+            Console.SetCursorPosition(x, y); // position
+            Console.Write(sym);
+            //end part code
+        }
+    }
+}
---
+++ snake/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Snake_OPP_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*that part of code is incapsulation, because we use "point" but didt see how it works
+            //put new values
+            Point p1 = new Point(1,3,'*');//we send values to the parametrs in the class point
+            p1.Draw(); 
+            // print the dot 
+            //second dot
+            Point p2 = new Point(5, 4, '#');//we send values to the parametrs in the class point
+            p2.Draw(); 
+            // print the dot 
+
+
+
+            ////this part of code make a dot in the place that the want to put it
+            ////another dot
+            Point p2 = new Point();
+            //put new values
+            p2.x = 5; //we send values to the parametrs in the class point
+            p2.y = 4;
+            p2.sym = '#';
+            p1.Draw(); // now we dint put a values into method because we use parametrs from class point
+            // print the dot 
+            ////end part code
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
